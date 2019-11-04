318f4fa3937bbc510518b640f37931dd3899565f
yuki <yuki097633@yahoo.co.jp>
2019-11-04 18:49:25 +0900

    create rgb.rb

---
+++ rgb.rb
@@ -0,0 +1,12 @@
+def to_hex(r,g,b)
+  [r,g,b].inject('#') do |hex,n|
+    hex + n.to_s(16).rjust(2,'0')
+  end
+end
+
+def to_ints(hex)
+  r,g,b = hex[1..2],hex[3..4],hex[5..6]
+  [r,g,b].map do |s|
+    s.hex
+  end
+end
