4524a94590a28651677f3d878c6ad6a77d5e22cc
yuki <yuki097633@yahoo.co.jp>
2019-11-09 00:27:07 +0900

    commit

--- gate.rb
+++ gate.rb
@@ -1,5 +1,29 @@
 class Gate
+	STATIONS = [:umeda,:juso,:mikuni]
+	FARES    = [150,190]
+
 	def initialize(name)
 		@name = name
 	end
+
+	def enter(ticket)
+		ticket.stump(@name)
+	end
+
+	def calc_fare(ticket)
+		from = STATIONS.index(ticket.stump_at)
+		to	 = STATIONS.index(@name)
+		distance = to - from
+		FARES[distance - 1]
+	end
+
+	def exit(ticket)
+		fare = calc_fare(ticket)
+		fare <= ticket.fare
+	end
+
+
+
+
+
 end
--- ticket.rb
+++ ticket.rb
@@ -1,5 +1,11 @@
 class Ticket
+	attr_reader :fare,:stump_at
+
 	def initialize(fare)
 	  @fare = fare
+	end
+
+	def stump(name)
+		@stump_at = name
 	end
 end
